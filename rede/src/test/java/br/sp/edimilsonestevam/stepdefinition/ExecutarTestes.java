2343be18e5a11f5e2309e79201f63bdfdaead16f
edimilsonestevam <edimilsonestevam@gmail.com>
2018-08-01 16:49:47 -0300

    Laboratório de automação de testes
    Ajustes na classe ExecutarTestes.

--- rede/src/test/java/br/sp/edimilsonestevam/stepdefinition/ExecutarTestes.java
+++ rede/src/test/java/br/sp/edimilsonestevam/stepdefinition/ExecutarTestes.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/br/sp/edimilsonestevam/feature"}
-                , glue = { "src/test/java/br/sp/edimilsonestevam/stepdefinition" }
+                , glue = { "src/test/java/br/sp/edimilsonestevam/stepDefinition" }
 				, dryRun = false
 			    )
 
